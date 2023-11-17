227199111cf47e1fe4a7035d2f789973af529c49
chuck roth <chuckroth42@gmail.com>
2023-11-16 22:50:41 -0500

    navbar was hardcoded omg

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { Anchor, Container} from 'atomize'
+import {Link} from 'react-router-dom'
 import lifer from '../components/image 12.png'
 import background from "../SkyGradient_top_mid.png";
 import { ShopContext } from '../context/shopContext';
@@ -12,13 +13,18 @@ const Navbar = () =>{
     return (
         <>
             <Container h="4rem" align="left" d="flex" flexDir="row" p="1rem" justify="space-between" pos="fixed" top="0px" style={{zIndex: "100"}} bgImg={background} minW="100%" >
-            <a href='http://localhost:3000/'><img src={lifer} alt="lifer" height="100%" ></img></a>
-            {/*<a href="http://localhost:3000/shop" className='navtag'>Shop</a>*/}
-            <a href="http://localhost:3000/about" className='navtag'>About</a>
-            {/*<a href="http://localhost:3000" className='navtag'>Archive</a>*/}
-            <a href="http://localhost:3000/gkys" className='navtag'>Home Page</a>
-            {/*<a href="http://localhost:3000/test" className='navtag'>Test Page</a>*/}
-            <a href="http://localhost:3000/Tshirt" className='navtag'>T Shirts</a>
+            <Link to="/">
+            <img src={lifer} alt="lifer" height="100%" />
+            </Link>
+            <Link to="/about" className="navtag">
+            About
+            </Link>
+            <Link to="/gkys" className="navtag">
+            Home Page
+            </Link>
+            <Link to="/Tshirt" className="navtag">
+            T Shirts
+            </Link>
             <Anchor h= "1.5rem" onClick={()=> openCart()} className='navtag' textColor="black">Cart</Anchor>
             </Container>
         </>
